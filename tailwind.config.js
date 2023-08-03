6e1dba522e73f1db3c50663f354c7fded7afd40e
Nyleco-J <anuran.nyleco24@gmail.com>
2023-08-03 15:45:30 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      'dark' : '#2b2b2b',
+      'buck' : '#3EB489'
+    },
+  },
+  plugins: [],
+}
+
