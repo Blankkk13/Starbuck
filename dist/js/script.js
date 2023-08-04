ea01593875c0a64c3d53a0530106879f5c54ffde
Nyleco-J <anuran.nyleco24@gmail.com>
2023-08-04 09:16:54 +0800

    sbbb

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,6 @@
-const toggleMenu = document.querySelector(".toggle__menu");
 const headerNav = document.querySelector(".header__nav");
-
+const toggleMenu = document.querySelector(".toggle__menu");
 toggleMenu.addEventListener("click", () => {
- headerNav.classList.toggle("open");
+    toggleMenu.classList.toggle("open");
+    headerNav.classList.toggle("open");
 });
